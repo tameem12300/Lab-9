3c21a52fd1edc1c5c6123361142ba4927b6cb1f1
tameem bourgi <bourgitameem429@gmail.com>
2024-05-12 04:47:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,46 @@
-from flask import Flask, render_template, request
+import flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-
-app = Flask('Furniture store')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Books.db'
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-
-# products = [
-#     {'prod_name': 'sofa',
-#      'price': 12000,
-#      'in_stock': False,
-#      'id': 0},
-#     {'prod_name': 'table',
-#      'price': 6000,
-#      'in_stock': True,
-#      'id': 1},
-#     {'prod_name': 'chair',
-#      'price': 8000,
-#      'in_stock': False,
-#      'id': 2},
-# ]
-
-
-class Product(db.Model):
+class StepsNote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    prod_name = db.Column(db.String(300))
-    price = db.Column(db.Integer)
-    in_stock = db.Column(db.Boolean, default=True)
+    steps = db.Column(db.Integer, nullable=True)
+    date = db.Column(db.String(10), nullable=False)
 
-    def __repr__(self):
-        return f'Product{self.id}. {self.prod_name} - {self.price} rub.'
+@app.route('/', methods=['GET'])
+def index():
+    # Fetching all step notes and reversing the order for display
+    step_notes = StepsNote.query.order_by(StepsNote.id.desc()).all()
+    # Calculating the total number of steps
+    total_steps = sum(note.steps for note in step_notes) if step_notes else 0
+    return flask.render_template('index.html', step_notes=step_notes, total_steps=total_steps)
 
+@app.route('/add_step', methods=['POST'])
+def add_step():
+    try:
+        steps = int(flask.request.form['step'])  
+        date = flask.request.form['date']
+        if steps > 0:
+            new_step_note = StepsNote(steps=steps, date=date)
+            db.session.add(new_step_note)
+            db.session.commit()
+        else:
+            flask.flash('Number of steps must be greater than zero.')
+    except ValueError:
+        flask.flash('Please enter a valid number for steps.')
+    return flask.redirect(flask.url_for('index'))
 
-@app.route('/')
-def main():
-    products = Product.query.all()
-    return render_template('index.html', products_list=products)
-
-
-@app.route('/in_stock/<product_id>', methods=['PATCH'])
-def modify_product(product_id):
-    product = Product.query.get(product_id)
-    product.in_stock = request.json['in_stock']
+@app.route('/delete_all', methods=['GET'])
+def delete_all():
+    StepsNote.query.delete()
     db.session.commit()
-    # global products
-    # in_stock = request.json['in_stock']
-    # for product in products:
-    #     if product['id'] == product_id:
-    #         product.update({'in_stock': in_stock})
-    # return 'OK'
+    return flask.redirect(flask.url_for('index'))
 
+with app.app_context():
+    db.create_all()
 
-@app.route('/add', methods=['POST'])
-def add_product():
-    data = request.json
-    product = Product(**data)
-    db.session.add(product)
-    db.session.commit()
-
-    # id_last = products[-1]['id']
-    # id_new = id_last + 1
-    # data['id'] = id_new
-    # products.append(data)
-    return 'OK'
-
-
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+if __name__ == "__main__":
     app.run(debug=True)
